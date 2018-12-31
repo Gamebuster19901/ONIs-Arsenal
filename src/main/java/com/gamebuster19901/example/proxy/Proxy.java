c7c531b7bc94d518a46187183868d19d5c0675b7
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2018-12-31 03:15:33 -0500

    Suppress Warnings

--- src/main/java/com/gamebuster19901/example/proxy/Proxy.java
+++ src/main/java/com/gamebuster19901/example/proxy/Proxy.java
@@ -6,15 +6,17 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public abstract class Proxy {
 	
+	@SuppressWarnings("unused")
 	public void preInit(FMLPreInitializationEvent e){
 		
 	}
 	
-	
+	@SuppressWarnings("unused")
 	public void init(FMLInitializationEvent e){
 		
 	}
 	
+	@SuppressWarnings("unused")
 	public void postInit(FMLPostInitializationEvent e){
 		
 	}
