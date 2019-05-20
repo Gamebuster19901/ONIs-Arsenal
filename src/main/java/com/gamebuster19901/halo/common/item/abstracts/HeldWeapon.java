8be187f5a1157931509664242afb07a47913f1a8
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2019-05-20 15:56:42 -0400

    Weapons now update

--- src/main/java/com/gamebuster19901/halo/common/item/abstracts/HeldWeapon.java
+++ src/main/java/com/gamebuster19901/halo/common/item/abstracts/HeldWeapon.java
@@ -4,9 +4,11 @@ import com.gamebuster19901.halo.common.item.HaloItem;
 import com.gamebuster19901.halo.common.item.capability.Weapon;
 import com.gamebuster19901.halo.common.item.capability.WeaponDefaultImpl;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 
@@ -39,6 +41,18 @@ public abstract class HeldWeapon extends HaloItem implements Weapon.Tag{
 		};
 	}
 	
-	
+    /**
+     * Called each tick as long the item is on a player inventory. Uses by maps to check if is on a player hand and
+     * update it's contents.
+     */
+	@Override
+	public void onUpdate(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected) {
+		Capability<Weapon> weapon = WeaponDefaultImpl.CAPABILITY;
+		if(isSelected) {
+			if(stack.hasCapability(weapon, EnumFacing.DOWN)) {
+				stack.getCapability(weapon, EnumFacing.DOWN).update();
+			}
+		}
+	}
 	
 }
