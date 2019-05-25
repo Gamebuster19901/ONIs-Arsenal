d40d8c3ab19ff57f9a4e69988f4e5c754c90c3a7
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2019-05-25 13:46:58 -0400

    remove unused class

--- src/main/java/com/gamebuster19901/halo/common/item/capability/multiple/MultipleCapabilities.java
+++
@@ -1,5 +0,0 @@
-package com.gamebuster19901.halo.common.item.capability.multiple;
-
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
-
-public interface MultipleCapabilities extends ICapabilityProvider{}
