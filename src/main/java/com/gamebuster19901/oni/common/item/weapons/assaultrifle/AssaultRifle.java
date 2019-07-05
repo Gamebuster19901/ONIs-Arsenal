2d09c380d6c5ee46cf4e3daa0a31fe29d79f05c9
Gamebuster1990 <Gamebuster19901@users.noreply.github.com>
2019-07-05 17:12:17 -0400

    Licensing and other legal stuff

--- src/main/java/com/gamebuster19901/oni/common/item/weapons/assaultrifle/AssaultRifle.java
+++ src/main/java/com/gamebuster19901/oni/common/item/weapons/assaultrifle/AssaultRifle.java
@@ -1,3 +1,20 @@
+/*
+ * Oni's Arsenal Copyright 2019 Gamebuster19901
+ * 
+ * Halo Copyright Microsoft Corporation
+ *
+ * Oni's Arsenal was created under Microsoft's
+ * "Game Content Usage Rules", using assets
+ * based on the Halo universe. Oni's Arsenal
+ * is not endorsed by or affiliated with
+ * Microsoft.
+ * 
+ * The Game Content Usage Rules can be found at:
+ * 
+ * https://www.xbox.com/en-us/developers/rules
+ * 
+ */
+
 package com.gamebuster19901.oni.common.item.weapons.assaultrifle;
 
 import com.gamebuster19901.guncore.capability.client.item.overlay.OverlayDefaultImpl;
