040f1bf786d46fca918028c2ac184e2407353783
Gamebuster19901 <Gamebuster19901@users.noreply.github.com>
2021-03-27 20:46:37 -0400

    Fabric

---
+++ src/main/java/com/gamebuster19901/oni/ONI.java
@@ -0,0 +1,17 @@
+package com.gamebuster19901.oni;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ONI implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+		int i = 0;
+		do {
+			System.out.println("LELELE");
+			i++;
+		}
+		while(i < 100);
+	}
+
+}
